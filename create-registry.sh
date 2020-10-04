0c30a7066f07538b12c1a076505e89f097e937cd
GNU Shim <geunwoo.j.shim@gmail.com>
2020-10-04 18:44:06 +0900

    Update create-registry.sh

--- create-registry.sh
+++ create-registry.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/env bash
 export store=/etc/docker/certs.d/192.168.1.10:8443
 openssl req -x509 -nodes -newkey rsa:4096 -keyout tls.key -out tls.crt -days 365 \
  -config tls.req -extensions v3_req
